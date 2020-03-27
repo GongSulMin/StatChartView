242bd2682f83bd0b8dc524be8e866fa74f5b7ba7
GongSulMin <tv302@naver.com>
2020-03-27 23:57:27 +0900

    ViewContract 수정

--- app/src/main/java/com/gong/statchartview/statchartview/ChartViewContract.kt
+++ app/src/main/java/com/gong/statchartview/statchartview/ChartViewContract.kt
@@ -1,9 +1,16 @@
 package com.gong.statchartview.statchartview
 
+import android.graphics.Path
+import com.gong.statchartview.statchartview.option.LineOption
+
 interface ChartViewContract {
 
     fun invalidate()
     fun postInvalidate()
-    fun drawLine(points: List<StatChartViewPoints>)
+    fun drawLine(points: List<StatChartViewPoints>, lineOption: LineOption)
+    fun drawLine(
+        path: Path,
+        lineOption: LineOption
+    )
 
 }
